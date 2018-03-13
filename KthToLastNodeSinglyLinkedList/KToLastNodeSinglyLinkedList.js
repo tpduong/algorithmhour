68698385ef0052bb9d771168010cff5e502bc0dd
Tracy Duong <tduong@mz.com>
2018-03-13 12:30:52 -0700

    KToLastNodeSinglyLinkedList solution

--- KthToLastNodeSinglyLinkedList/KToLastNodeSinglyLinkedList.js
+++ KthToLastNodeSinglyLinkedList/KToLastNodeSinglyLinkedList.js
@@ -22,3 +22,34 @@
  kthToLastNode(2, a);
  // returns the node with value "Devil's Food" (the 2nd to last node)
  */
+
+function kthToLastNode(k, headNode) {
+  var kNodes = [];
+  var currentNode = headNode;
+  while (currentNode !== null) {
+    if (kNodes.length >= k) {
+      kNodes.shift();
+    }
+    kNodes.push(currentNode)
+    currentNode = currentNode.next;
+  }
+  return kNodes[0];
+}
+
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
+var a = new LinkedListNode("Angel Food");
+var b = new LinkedListNode("Bundt");
+var c = new LinkedListNode("Cheese");
+var d = new LinkedListNode("Devil's Food");
+var e = new LinkedListNode("Eccles");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(2, a));
