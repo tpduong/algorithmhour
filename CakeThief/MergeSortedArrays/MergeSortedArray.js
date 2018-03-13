fb9fafff78fa99819776fedcddf74c1c24092aec
Tracy Duong <tduong@mz.com>
2018-03-12 17:17:33 -0700

    Added MergedSortedArray problem

---
+++ CakeThief/MergeSortedArrays/MergeSortedArray.js
@@ -0,0 +1,5 @@
+// var myArray     = [3, 4, 6, 10, 11, 15];
+// var alicesArray = [1, 5, 8, 12, 14, 19];
+//
+// console.log(mergeArrays(myArray, alicesArray));
+// logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
