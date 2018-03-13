d464f66a51b255798f99da22d9ba0fac723367c2
Tracy Duong <tduong@mz.com>
2018-03-12 17:34:34 -0700

    Added kthToLastNode

---
+++ KthToLastNodeSinglyLinkedList/KToLastNodeSinglyLinkedList.js
@@ -0,0 +1,24 @@
+/*
+ Write a function kthToLastNode() that takes an integer kk and the headNode of a singly-linked list, and returns the kkth to last node in the list.
+
+ For example:
+
+ function LinkedListNode(value) {
+ this.value = value;
+ this.next = null;
+ }
+
+ var a = new LinkedListNode("Angel Food");
+ var b = new LinkedListNode("Bundt");
+ var c = new LinkedListNode("Cheese");
+ var d = new LinkedListNode("Devil's Food");
+ var e = new LinkedListNode("Eccles");
+
+ a.next = b;
+ b.next = c;
+ c.next = d;
+ d.next = e;
+
+ kthToLastNode(2, a);
+ // returns the node with value "Devil's Food" (the 2nd to last node)
+ */
